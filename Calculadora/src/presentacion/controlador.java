1403a2fcce2615f2992143006ad3abc4ecf96d2e
Ginneidy <64049555+Ginneidy@users.noreply.github.com>
2020-10-21 16:05:39 -0500

    Botones2

--- Calculadora/src/presentacion/controlador.java
+++ Calculadora/src/presentacion/controlador.java
@@ -43,9 +43,46 @@ public class controlador implements ActionListener{
         if (e.getSource() == view.getNineButton()){
             
         }
+        if (e.getSource() == view.getPointButton()){
+
+        }
         if (e.getSource() == view.getSumButton()){
 
         }
+        if (e.getSource() == view.getSubtrButton()){
+
+        }
+        if (e.getSource() == view.getMultButton()){
+
+        }
+        if (e.getSource() == view.getDiviButton()){
+
+        }
+        if (e.getSource() == view.getSumButton()){
+
+        }
+        if (e.getSource() == view.getCEButton()){
+
+        }
+        if (e.getSource() == view.getCButton()){
+
+        }
+        if (e.getSource() == view.getExponButton()){
+
+        }
+        if (e.getSource() == view.getReverseButton()){
+
+        }
+        if (e.getSource() == view.getPercentButton()){
+
+        }
+        if (e.getSource() == view.getSqrtButton()){
+
+        }
+        if (e.getSource() == view.getEqualButton()){
+
+        }
+
     }
     
 }
